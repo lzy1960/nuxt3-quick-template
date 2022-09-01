c3500c329065241ace833d16acdd80d1cffded31
lzy1960 <1960553899@qq.com>
2022-09-01 10:05:41 +0800

    chore: init with unocss and vueuse

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style lang="scss">
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="default-page">
+    default page
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
